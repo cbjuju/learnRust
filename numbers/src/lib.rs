19959115d508f023d705337384554d47fdacd797
Sudip Kunda <sudipkunda@gmail.com>
2022-01-17 18:46:39 +0530

    Use & when dealing with vectors otherwise they will be consumed.

--- numbers/src/lib.rs
+++ numbers/src/lib.rs
@@ -1,3 +1,7 @@
-pub fn say_hello() {
-    println!("Hello, world!");
+pub fn print() {
+    let numbers = vec![1, 2, 3, 4, 5];
+    for n in &numbers {
+        println!("{:?}", n);
+    }
+    println!("The length of the vector is {:?}.", numbers.len());
 }
